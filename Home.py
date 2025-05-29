c68c3bdb96f3077714969587f4432b22d8b889a9
fitripl02 <fitripuji0212@students.unnes.ac.id>
2025-05-29 14:40:31 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
 st.title("Selamat Datang")
-st.write("Selamat datang di pusat informasi kinerja restoran Anda!
-Dashboard ini dirancang untuk membantu Anda memahami data operasional restoran secara menyeluruh dan mengambil keputusan berbasis data.")
+st.write("Selamat datang di pusat informasi kinerja restoran Anda! Dashboard ini dirancang untuk membantu Anda memahami data operasional restoran secara menyeluruh dan mengambil keputusan berbasis data.")
 
