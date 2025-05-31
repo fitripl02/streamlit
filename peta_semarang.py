3e84fd6455d3330be3ba59fb6ce2bbd45c44f095
fitripl02 <fitripuji0212@students.unnes.ac.id>
2025-05-31 21:27:41 +0700

    Update peta_semarang.py

--- peta_semarang.py
+++ peta_semarang.py
@@ -1,25 +1,8 @@
+import streamlit as st
 import pandas as pd
-import folium
-from folium.plugins import MarkerCluster
+import pydeck as pdk
 
-# Load data
 df = pd.read_csv("semarang_resto_dataset.csv")
 
-# Validasi kolom
-if not {'nama_resto', 'latitude', 'longitude'}.issubset(df.columns):
-    raise ValueError("Dataset harus memiliki kolom: nama_resto, latitude, longitude")
-
-# Buat peta
-map_center = [-6.9667, 110.4167]
-m = folium.Map(location=map_center, zoom_start=12)
-marker_cluster = MarkerCluster().add_to(m)
-
-for _, row in df.iterrows():
-    folium.Marker(
-        location=[row['latitude'], row['longitude']],
-        popup=row['nama_resto'],
-        icon=folium.Icon(color='blue', icon='cutlery', prefix='fa')
-    ).add_to(marker_cluster)
-
-m.save("peta_restoran_semarang.html")
-print
+st.header("Peta Sebaran Restoran di Semarang")
+st.map(df[['latitude', 'longitude']])
