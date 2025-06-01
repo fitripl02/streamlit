e7e6295e2a354efde57b125b199bced381e4a3b9
fitripl02 <fitripuji0212@students.unnes.ac.id>
2025-06-01 11:17:30 +0700

    Delete peta_semarang.py

--- peta_semarang.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-import pydeck as pdk
-
-df = pd.read_csv("semarang_resto_dataset.csv")
-
-st.header("Peta Sebaran Restoran di Semarang")
-st.map(df[['latitude', 'longitude']])
