2bde8f1655eb3a32e5e035a2f46aa1ef13e94bf7
fitripl02 <fitripuji0212@students.unnes.ac.id>
2025-06-02 18:23:37 +0700

    Update 2_Data Visualization.py

--- pages/2_Data Visualization.py
+++ pages/2_Data Visualization.py
@@ -28,9 +28,9 @@ if "resto_type" in df.columns:
     st.subheader("🍽️ Jenis Restoran Paling Umum")
     st.bar_chart(df["resto_type"].value_counts().head(10))
 
-   if len(df.columns) > 4:
+if len(df.columns) > 4:
     st.warning("Pair plot dengan banyak variabel mungkin sulit dibaca. Pertimbangkan untuk memilih subset data.")
-
+     
 if __name__ == "__main__":
     main()
 
